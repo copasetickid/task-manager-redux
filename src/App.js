396b51819b7df8b54ba84aa747a2018f8935f4b6
Rushaine McBean <rushaine.mcbean@gmail.com>
2018-01-04 00:49:45 -0500

    Start of adding in redux

--- src/App.js
+++ src/App.js
@@ -3,21 +3,6 @@ import TasksPage from './components/TasksPage';
 import logo from './logo.svg';
 import './App.css';
 
-const mockTasks = [
-  {
-    id: 1,
-    title: 'Learn Redux',
-    description: 'The store, action and reducers, oh my!',
-    status: 'In Progress'
-  },
-  {
-    id: 2,
-    title: 'Peace on Earth',
-    description: 'No big deal',
-    status: 'In Progress'
-  },
-];
-
 class App extends Component {
   render() {
     return (
